7239ceec7247c7262347a4ffb6400bb3480dcd20
slaw-ik <slaw-ik@yandex.ru>
2013-11-26 00:23:23 +0200

    add_article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,7 @@ class Article < ActiveRecord::Base
   attr_accessible :content, :title
 
   has_many :comments
+
+  validates :title, :presence => true
+  validates :content, :presence => true
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ActiveRecord::Base
   attr_accessible :article_id, :author, :content
 
   belongs_to :article
+
+  validates :author, :presence => true
+  validates :content, :presence => true
 end
